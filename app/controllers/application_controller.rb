77d8dfbde4094b44fd42a18fae5751f14972c879
tujal.logicalstreet <tujal.logicalstreet@gmail.com>
2024-03-15 11:08:47 +0530

    chnages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ before_action :authenticate_user!
 before_action :configure_permitted_parameters, if: :devise_controller?
 protected
     def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:username,:first_name, :last_name, :email,:gender, :password, :password_confirmation)}
                  devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:profile_image, :current_password)} 
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_14_104335) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_15_043310) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -96,6 +96,10 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_14_104335) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_name"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "gender"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
