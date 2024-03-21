399d1fec1f82a5d24bc81079be679c7566229e35
tujal.logicalstreet <tujal.logicalstreet@gmail.com>
2024-03-21 17:15:37 +0530

    add validation in story and reel model

--- app/models/reel.rb
+++ app/models/reel.rb
@@ -2,4 +2,16 @@ class Reel < ApplicationRecord
   has_one_attached :video
   has_many :likes , as: :likable
   belongs_to :user
+  validate :check_video_type
+
+    private
+    def check_video_type
+        if video.attached?
+            if video.content_type == "application/pdf" || video.content_type =="image/jpg" || video.content_type =="image/png" || video.content_type =="image/jpeg"
+                errors.add(:video , "Plz upload mp4 file ")
+            end
+        else
+            errors.add(:video, "video can't be blank")
+        end
+    end
 end
--- app/models/story.rb
+++ app/models/story.rb
@@ -2,5 +2,16 @@ class Story < ApplicationRecord
   has_one_attached :story
   has_many :likes , as: :likable
   belongs_to :user
-  validates :story, presence: true
+  validate :check_story_type
+
+    private
+    def check_story_type
+        if story.attached?
+            if story.content_type == "application/pdf" || story.content_type =="image/jpg" || story.content_type =="image/png" || story.content_type =="image/jpeg"
+                errors.add(:story , "Plz upload mp4 file ")
+            end
+        else
+            errors.add(:story, "Story can't be blank")
+        end
+    end
 end
