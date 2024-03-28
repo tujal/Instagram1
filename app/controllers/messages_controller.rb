99a49be2ecc43833f08b33e1325a6523cd6df969
tujal.logicalstreet <tujal.logicalstreet@gmail.com>
2024-03-28 14:23:15 +0530

    changes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,6 +10,16 @@ class MessagesController < ApplicationController
         end
     end
 
+    def destroy
+        @group = Group.find(params[:group_id])
+        @message = @group.messages.find(params[:id])
+        @message.destroy
+          respond_to do | format |
+              format.turbo_stream { render turbo_stream: turbo_stream.remove("message_#{@message.id}")}
+              format.html {redirect_to user_group_path( current_user, @group)}
+        end
+    end
+
 
     private
     def messages_params
