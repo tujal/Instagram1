bb8d931da00254d0c74584f8d5d3ab4dbb19e018
tujal.logicalstreet <tujal.logicalstreet@gmail.com>
2024-03-28 12:45:45 +0530

    changes

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -22,6 +22,27 @@ class GroupsController < ApplicationController
     end
 
 
+    def edit
+        @group = current_user.groups.find(params[:id])
+    end
+
+    def update
+        @group = current_user.groups.find(params[:id])
+        if @group.update(groups_params)
+            redirect_to user_group_path(current_user, @group)
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @group = current_user.groups.find(params[:id])
+        @group.destroy
+        redirect_to  user_groups_path
+    end
+
+
+
     private
     def groups_params
         params.require(:group).permit(:name, :group_image, :user_id, :bio)
